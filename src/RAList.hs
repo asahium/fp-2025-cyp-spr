8099458588468bb60cf1d36b444d6ea2a0e74c5f
Biktimirov Danila <psiwyrm@tuta.io>
2025-03-13 16:58:10 +0200

    Update RAList.hs

--- src/RAList.hs
+++ src/RAList.hs
@@ -1,7 +1,6 @@
 module RAList where
 
 import qualified Tree as T
-module Tree (fromList, splitAtLevels, null, singleton, deleteMin) where
 
 -- This is an implementation of a random-access list.
 -- It's possible to achieve logarithmic access by representing a list as a sequence of perfect trees.
@@ -24,25 +23,28 @@ type RAList a = [Digit a]
 
 -- Checks that the random-access list has correct structure
 wellFormed :: RAList a -> Bool
-wellFormed = all (either (const True) T.wellFormed) . reverse
+wellFormed = all (\x -> case x of One t -> T.wellFormed t; Zero -> True)
 
 -- Flattens a random-access list into a normal list
 toList :: RAList a -> [a]
-toList = concatMap (either (const []) T.toList) . reverse
+toList = concatMap (\x -> case x of One t -> T.toList t; Zero -> [])
 
 -- Generates a random-access list from a normal list
 fromList :: [a] -> RAList a
-fromList = map (One . T.fromList) . T.splitAtLevels . length
+fromList [] = []
+fromList xs = foldr cons nil xs
 
 -- Fetches the k-th element of the list
 fetch :: Int -> RAList a -> a
-fetch k xs = case xs of
-  Zero : _ -> error "Index out of bounds"
-  One t : _ -> T.fetch k t
+fetch _ [] = error "Index out of bounds"
+fetch k (Zero : xs) = fetch k xs
+fetch k (One t : xs)
+  | k < T.size t = T.fetch k t
+  | otherwise    = fetch (k - T.size t) xs
 
 -- Checks the list for emptiness
 null :: RAList a -> Bool
-null = all (either (const True) T.null) . reverse
+null = all (== Zero)
 
 -- Creates the empty list
 nil :: RAList a
@@ -51,17 +53,22 @@ nil = []
 -- Adds a given element to the head of the list.
 -- Should always return a well-formed list.
 cons :: a -> RAList a -> RAList a
-cons h t = One (T.singleton h) : t
+cons x (One t1 : One t2 : ts) = Zero : cons x (One (T.node t1 t2) : ts)
+cons x ts = One (T.Leaf x) : ts
 
 -- Splits the list into a head and a tail, if not empty.
 -- The resulting tail should be a well-formed list.
 uncons :: RAList a -> Maybe (a, RAList a)
-uncons xs = case xs of
-  Zero : _ -> Nothing
-  One t : ts -> Just (T.fetch 0 t, One (T.deleteMin t) : ts)
+uncons [] = Nothing
+uncons (Zero : ts) = uncons ts
+uncons (One t : ts) =
+  let (x:xs) = T.toList t
+  in Just (x, if null xs then Zero : ts else One (T.fromList xs) : ts)
 
 -- Updates the k-th element of the list with the given value
 update :: Int -> a -> RAList a -> RAList a
-update n x xs = case xs of
-  Zero : _ -> error "Index out of bounds"
-  One t : ts -> One (T.update n x t) : ts
+update _ _ [] = error "Index out of bounds"
+update k x (Zero : xs) = Zero : update k x xs
+update k x (One t : xs)
+  | k < T.size t = One (T.update k x t) : xs
+  | otherwise    = One t : update (k - T.size t) x xs
